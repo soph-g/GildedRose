f6e756b06966aeb4b767d616d72f651532644ada
Sophie Gill <sophie-gill@hotmail.com>
2018-11-28 18:53:17 +0000

    moves quality updates into an if else to reduce branching

--- gilded_rose.rb
+++ gilded_rose.rb
@@ -19,25 +19,27 @@ class GildedRose
       if (item.name == "Sulfuras, Hand of Ragnaros")
         next
       end
-      if (item.name != "Aged Brie" && item.name != "Backstage passes to a TAFKAL80ETC concert")
-        if (item.quality > 0)
-          item.quality = item.quality - 1
-        end
-      else
+      if (item.name == "Aged Brie")
         if (item.quality < 50)
           item.quality = item.quality + 1
-          if (item.name == "Backstage passes to a TAFKAL80ETC concert")
-            if (item.sell_in < 11)
-              if (item.quality < 50)
-                item.quality = item.quality + 1
-              end
-            end
-            if (item.sell_in < 6)
-              if (item.quality < 50)
-                item.quality = item.quality + 1
-              end
+        end
+      elsif (item.name == "Backstage passes to a TAFKAL80ETC concert")
+        if (item.quality < 50)
+          item.quality = item.quality + 1
+          if (item.sell_in < 11)
+            if (item.quality < 50)
+              item.quality = item.quality + 1
             end
           end
+          if (item.sell_in < 6)
+            if (item.quality < 50)
+              item.quality = item.quality + 1
+            end
+          end
+        end
+      else
+        if (item.quality > 0)
+          item.quality = item.quality - 1
         end
       end
       item.sell_in = item.sell_in - 1;
