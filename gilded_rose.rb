8001cd88691496cab3ddf958e8449ae39df2f209
Sophie Gill <sophie-gill@hotmail.com>
2018-11-28 18:59:07 +0000

    refactors increase in quality for Aged Brie and concert tickets

--- gilded_rose.rb
+++ gilded_rose.rb
@@ -19,23 +19,15 @@ class GildedRose
       if (item.name == "Sulfuras, Hand of Ragnaros")
         next
       end
-      if (item.name == "Aged Brie")
-        if (item.quality < 50)
-          item.quality = item.quality + 1
-        end
+      if (item.name == "Aged Brie" && item.quality < 50)
+        item.quality = item.quality + 1
       elsif (item.name == "Backstage passes to a TAFKAL80ETC concert")
-        if (item.quality < 50)
+        if (item.sell_in < 6 && item.quality < 50)
+          item.quality = item.quality + 3
+        elsif (item.sell_in < 11 && item.quality < 50)
+          item.quality = item.quality + 2
+        elsif (item.quality < 50)
           item.quality = item.quality + 1
-          if (item.sell_in < 11)
-            if (item.quality < 50)
-              item.quality = item.quality + 1
-            end
-          end
-          if (item.sell_in < 6)
-            if (item.quality < 50)
-              item.quality = item.quality + 1
-            end
-          end
         end
       else
         if (item.quality > 0)
