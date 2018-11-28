965771f1cc8282e7b92d394913d53809881ede7d
Sophie Gill <sophie-gill@hotmail.com>
2018-11-28 16:18:55 +0000

    refactors out an increase_quality method

--- gilded_rose.rb
+++ gilded_rose.rb
@@ -25,16 +25,16 @@ class GildedRose
         end
       else
         if (@items[i].quality < 50)
-          @items[i].quality = @items[i].quality + 1
+          increase_quality(@items[i], 1)
           if (@items[i].name == "Backstage passes to a TAFKAL80ETC concert")
             if (@items[i].sell_in < 11)
               if (@items[i].quality < 50)
-                @items[i].quality = @items[i].quality + 1
+                increase_quality(@items[i], 1)
               end
             end
             if (@items[i].sell_in < 6)
               if (@items[i].quality < 50)
-                @items[i].quality = @items[i].quality + 1
+                increase_quality(@items[i], 1)
               end
             end
           end
@@ -56,11 +56,17 @@ class GildedRose
           end
         else
           if (@items[i].quality < 50)
-            @items[i].quality = @items[i].quality + 1
+            increase_quality(@items[i], 1)
           end
         end
       end
     end
+  end
+
+  private
+
+  def increase_quality(item, amount)
+    item.quality += amount
   end
 
 end
