22cc69d27ca27c31223b6d78bcfe9596f9e50bc2
Sophie Gill <sophie-gill@hotmail.com>
2018-11-28 19:18:26 +0000

    moves sell_in reduction to top for AgedBrie

--- gilded_rose.rb
+++ gilded_rose.rb
@@ -19,8 +19,14 @@ class GildedRose
       if (item.name == "Sulfuras, Hand of Ragnaros")
         next
       end
-      if (item.name == "Aged Brie" && item.quality < 50)
-        item.quality = item.quality + 1
+      item.sell_in = item.sell_in - 1 if item.name == "Aged Brie"
+      if (item.name == "Aged Brie")
+        if (item.quality < 50)
+          item.quality = item.quality + 1
+          if (item.sell_in < 0 && item.quality < 50)
+            item.quality = item.quality + 1
+          end
+        end
       elsif (item.name == "Backstage passes to a TAFKAL80ETC concert" && item.quality < 50)
         item.quality = item.quality + 1
         if (item.sell_in < 6 && item.quality < 50)
@@ -34,15 +40,15 @@ class GildedRose
           item.quality = item.quality - 1
         end
       end
-      item.sell_in = item.sell_in - 1;
+      item.sell_in = item.sell_in - 1 if item.name != "Aged Brie"
       if (item.sell_in < 0)
         if (item.name == "Backstage passes to a TAFKAL80ETC concert")
           item.quality = item.quality - item.quality
-        elsif (item.name == "Aged Brie")
-          if (item.quality < 50)
-            item.quality = item.quality + 1
-          end
-        elsif (item.quality > 0)
+        # elsif (item.name == "Aged Brie")
+        #   if (item.quality < 50)
+        #     item.quality = item.quality + 1
+        #   end
+        elsif (item.name != "Aged Brie" && item.quality > 0)
           item.quality = item.quality - 1
         end
       end
