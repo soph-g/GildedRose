ffb24d4b0c1e6eabe621cd96fc9b125a91d1bdb1
Sophie Gill <sophie-gill@hotmail.com>
2018-11-28 19:05:06 +0000

    refactors post sell_in quality reduction to if else

--- gilded_rose.rb
+++ gilded_rose.rb
@@ -38,17 +38,12 @@ class GildedRose
       if (item.sell_in < 0)
         if (item.name == "Backstage passes to a TAFKAL80ETC concert")
           item.quality = item.quality - item.quality
-        end
-        if (item.name != "Aged Brie")
-          if (item.name != "Backstage passes to a TAFKAL80ETC concert")
-            if (item.quality > 0)
-              item.quality = item.quality - 1
-            end
-          end
-        else
+        elsif (item.name == "Aged Brie")
           if (item.quality < 50)
             item.quality = item.quality + 1
           end
+        elsif (item.quality > 0)
+          item.quality = item.quality - 1
         end
       end
     end
