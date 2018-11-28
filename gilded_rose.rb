e899745bb40084cd160d6389adab7830d036098c
Sophie Gill <sophie-gill@hotmail.com>
2018-11-28 19:03:17 +0000

    moves concert quality reset outside of branch

--- gilded_rose.rb
+++ gilded_rose.rb
@@ -36,13 +36,14 @@ class GildedRose
       end
       item.sell_in = item.sell_in - 1;
       if (item.sell_in < 0)
+        if (item.name == "Backstage passes to a TAFKAL80ETC concert")
+          item.quality = item.quality - item.quality
+        end
         if (item.name != "Aged Brie")
           if (item.name != "Backstage passes to a TAFKAL80ETC concert")
             if (item.quality > 0)
               item.quality = item.quality - 1
             end
-          else
-            item.quality = item.quality - item.quality
           end
         else
           if (item.quality < 50)
